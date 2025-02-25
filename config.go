b6d2250fe574b366a6a758d9fe1407244ed16ff6
Sergey Matov <sergey.matov@githedgehog.com>
2025-02-25 16:13:29 +0400

    Add Config

--- config.go
+++ config.go
@@ -8,111 +8,84 @@ import (
 	"time"
 )
 
-// BGP Configuration structure
-type BGPConfig struct {
-	MyASN       uint32
-	RouterID    string
-	Peers       []BGPPeer
-	Networks    []Network
-	RouteMaps   []RouteMap
-	PrefixLists []PrefixList
-	VRFs        []VRF
+// GlobalConfig structure
+type GlobalConfig struct {
+	ASN      uint16
+	LogLevel string
+	Routers  []RouterConfig
 }
 
-// BGP Peer structure
-type BGPPeer struct {
-	PeerASN     uint32
+// RouterConfig structure
+type RouterConfig struct {
+	VRF      string
+	RouterID string
+	Peers    []NeighborConfig
+}
+
+// NeighborConfig structure
+type NeighborConfig struct {
 	PeerIP      string
+	PeerASN     string
 	Password    string
 	Description string
-	RouteMapIn  string
-	RouteMapOut string
-	VRF         string
 }
 
-// Network structure for VRF support
-type Network struct {
-	Prefix string
-	VRF    string
-}
+/*const frrConfigTemplate = `
+frr defaults traditional
+hostname frr-k8s
+log syslog {{.LogLevel}}
 
-// Route Map structure
-type RouteMap struct {
-	Name   string
-	Permit bool
-	Seq    int
-	Prefix string
-}
+{{- range .Routers }}
+router bgp {{$.ASN}} vrf {{.VRF}}
+ bgp router-id {{.RouterID}}
+ bgp log-neighbor-changes
+ bgp graceful-restart
+ no bgp ebgp-requires-policy
+ no bgp network import-check
+ no bgp default ipv4-unicast
 
-// Prefix List structure
-type PrefixList struct {
-	Name   string
-	Seq    int
-	Permit bool
-	Prefix string
-}
+{{- range .Peers }}
+ neighbor {{.PeerIP}} remote-as {{.PeerASN}}
+{{- if .Password }}
+ neighbor {{.PeerIP}} password {{.Password}}
+{{- end }}
+{{- if .Description }}
+ neighbor {{.PeerIP}} description "{{.Description}}"
+{{- end }}
+{{- end }}
 
-// VRF structure
-type VRF struct {
-	Name string
-	RD   string // Route Distinguisher
-}
+ address-family ipv4 unicast
+{{- range .Peers }}
+  neighbor {{.PeerIP}} activate
+{{- end }}
+ exit-address-family
+
+exit
+{{- end }}
+`*/
 
 const frrConfigTemplate = `
 frr defaults traditional
 hostname frr-k8s
-log file /var/log/frr.log
+log syslog informational
 
-{% for vrf in .VRFs %}
-vrf {{vrf.Name}}
- rd {{vrf.RD}}
-exit
-{% endfor %}
+router bgp 64512
+ bgp router-id 192.168.1.1
+ bgp log-neighbor-changes
+ no bgp ebgp-requires-policy
+ no bgp network import-check
+ no bgp default ipv4-unicast
+ bgp graceful-restart
+ timers bgp 30 90
 
-router bgp {{.MyASN}}
- bgp router-id {{.RouterID}}
-
-{% for vrf in .VRFs %}
- address-family ipv4 vrf {{vrf.Name}}
+ address-family ipv4 unicast
  exit-address-family
-{% endfor %}
 
-{% for peer in .Peers %}
-{% if peer.VRF != "" %}
- address-family ipv4 vrf {{peer.VRF}}
-{% endif %}
- neighbor {{peer.PeerIP}} remote-as {{peer.PeerASN}}
-{% if peer.Password != "" %}
- neighbor {{peer.PeerIP}} password {{peer.Password}}
-{% endif %}
-{% if peer.Description != "" %}
- neighbor {{peer.PeerIP}} description "{{peer.Description}}"
-{% endif %}
-{% if peer.RouteMapIn != "" %}
- neighbor {{peer.PeerIP}} route-map {{peer.RouteMapIn}} in
-{% endif %}
-{% if peer.RouteMapOut != "" %}
- neighbor {{peer.PeerIP}} route-map {{peer.RouteMapOut}} out
-{% endif %}
-{% if peer.VRF != "" %}
+ address-family ipv6 unicast
  exit-address-family
-{% endif %}
-{% endfor %}
-
-{% for network in .Networks %}
-{% if network.VRF != "" %}
- address-family ipv4 vrf {{network.VRF}}
-{% endif %}
- network {{network.Prefix}}
-{% if network.VRF != "" %}
- exit-address-family
-{% endif %}
-{% endfor %}
-
-exit
 `
 
-func generateFRRConfig(config BGPConfig, outputPath string) error {
+func generateFRRConfig(config GlobalConfig, outputPath string) error {
 	// Parse the template
 	tmpl, err := template.New("frrConfig").Parse(frrConfigTemplate)
 	if err != nil {
@@ -126,6 +99,12 @@ func generateFRRConfig(config BGPConfig, outputPath string) error {
 	}
 	defer file.Close()
 
+	vtysh, err := os.Create("/etc/frr/vtysh.conf")
+	if err != nil {
+		return err
+	}
+	defer vtysh.Close()
+
 	// Execute template and write to file
 	err = tmpl.Execute(file, config)
 	if err != nil {
@@ -138,7 +117,7 @@ func generateFRRConfig(config BGPConfig, outputPath string) error {
 
 func reloadFRR() error {
 	fmt.Println("Reloading FRR configuration...")
-	cmd := exec.Command("/usr/lib/frr/frr-reload.py", "--reload")
+	cmd := exec.Command("/usr/lib/frr/frr-reload.py", "--reload", "--overwrite", "/etc/frr/frr.conf")
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		fmt.Println("Failed to reload FRR:", err, string(output))
@@ -149,35 +128,32 @@ func reloadFRR() error {
 }
 
 func main() {
-	// Sample BGP configuration
-	bgpConfig := BGPConfig{
-		MyASN:    64512,
-		RouterID: "192.168.1.1",
-		Peers: []BGPPeer{
-			{PeerASN: 64513, PeerIP: "192.168.1.2", Description: "Peer Router 1", RouteMapIn: "RM-IN", RouteMapOut: "RM-OUT", VRF: "CUSTOMER_A"},
-			{PeerASN: 64514, PeerIP: "192.168.1.3", Password: "securepass", VRF: "CUSTOMER_B"},
-		},
-		Networks: []Network{
-			{Prefix: "10.1.1.0/24", VRF: "CUSTOMER_A"},
-			{Prefix: "192.168.100.0/24", VRF: "CUSTOMER_B"},
-		},
-		PrefixLists: []PrefixList{
-			{Name: "PL-IN", Seq: 10, Permit: true, Prefix: "10.1.1.0/24"},
-			{Name: "PL-OUT", Seq: 20, Permit: false, Prefix: "192.168.100.0/24"},
-		},
-		RouteMaps: []RouteMap{
-			{Name: "RM-IN", Permit: true, Seq: 10, Prefix: "PL-IN"},
-			{Name: "RM-OUT", Permit: false, Seq: 20, Prefix: "PL-OUT"},
-		},
-		VRFs: []VRF{
-			{Name: "CUSTOMER_A", RD: "100:1"},
-			{Name: "CUSTOMER_B", RD: "200:1"},
+	// Sample configuration
+	globalConfig := GlobalConfig{
+		ASN:      64512,
+		LogLevel: "debug",
+		Routers: []RouterConfig{
+			{
+				VRF:      "GOVNO",
+				RouterID: "192.168.1.1",
+				Peers: []NeighborConfig{
+					{PeerIP: "192.168.1.2", PeerASN: "64513", Description: "DRUG GOVNA"},
+					{PeerIP: "192.168.1.3", PeerASN: "64514", Password: "zalupa"},
+				},
+			},
+			{
+				VRF:      "MOCHA",
+				RouterID: "192.168.2.1",
+				Peers: []NeighborConfig{
+					{PeerIP: "192.168.2.2", PeerASN: "64515", Description: "DRUG MOCHI"},
+				},
+			},
 		},
 	}
 
 	// Generate FRR config and reload FRR periodically
 	for {
-		err := generateFRRConfig(bgpConfig, "/etc/frr/frr.conf")
+		err := generateFRRConfig(globalConfig, "/etc/frr/frr.conf")
 		if err != nil {
 			fmt.Println("Error generating FRR config:", err)
 		} else {
